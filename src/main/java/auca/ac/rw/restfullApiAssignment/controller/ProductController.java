6bb37e7fe65b17180ee7ff34cf2d46c14ed9a981
Moïse <benineza.moise@gmail.com>
2026-02-18 17:20:48 +0200

    Update ProductController.java

--- src/main/java/auca/ac/rw/restfullApiAssignment/controller/ProductController.java
+++ src/main/java/auca/ac/rw/restfullApiAssignment/controller/ProductController.java
@@ -29,27 +29,23 @@ public class ProductController {
         }
     }
 
-    // Read All Products
     @GetMapping("/getAllProducts")
     public List<Product> getAllProducts() {
         return productserve.getAllProducts();
     }
 
-    // Update entire product
     @PutMapping("/updateProduct")
     public ResponseEntity<String> updateProduct(@RequestBody Product product) {
         String response = productserve.updateProduct(product);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    // Partial update: stock quantity only
     @PatchMapping("/updateProductStock/{id}/{stockQuantity}")
     public ResponseEntity<String> updateProductStock(@PathVariable Long id, @PathVariable int stockQuantity) {
         String response = productserve.updateProductStock(id, stockQuantity);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    // Delete Product
     @DeleteMapping("/deleteProduct/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
         String response = productserve.deleteProduct(id);
